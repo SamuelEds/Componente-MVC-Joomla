e645de0e9ffc47b17723065b766716fcaf436ddb
Samuel Sampaio <samueledshotmailgm@gmail.com>
2021-07-28 12:01:53 -0300

    Artigo 03 - Adicionando uma view para o site
    Adicionando uma view para o lado front-end.

--- site/helloworld.php
+++ site/helloworld.php
@@ -1,3 +1,19 @@
-<!--EXIBIR UM TEXTO SIMPLES NA TELA QUANDO O USUÁRIO ACESSAR O FRONT-END-->
+<!--ARQUIVO RESPONSÁVEL PELO CONTROLE DE EXECUÇÕES-->
 
-Um olá mundo para o cliente.
+<?php  
+
+//IMPDIR O ACESSO DIRETO.
+defined('_JEXEC') or die('Essa página não pode ser acessada diretamente');
+
+//INSTANCIAR O CONTROLADOR GERAL.
+//INFORMAR O NOME DO CONTROLADOR ENTRE PARÊNTESES.
+$controle = JControllerLegacy::getInstance('HelloWorld');
+
+//EXECUTAR SOLICITAÇÕES DE TAREFAS.
+$controle->execute(JFactory::getApplication()->input->getCmd('task'));
+
+//CONTROLE DE REDIRECIONAMENTO.
+//IRÁ REDIRECIONAR SE FOR SETADO PELO CONTROLADOR.
+$controle->redirect();
+
+?>
